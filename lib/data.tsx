3bdbb9db4f271556a56b6e5d365fb9071c6f8e17
Caleb <92544582+Oluwaferanmi-Dev@users.noreply.github.com>
2025-04-13 13:16:31 +0100

    projects

--- lib/data.tsx
+++ lib/data.tsx
@@ -1,156 +1,24 @@
-export const expereince = [
-  {
-    title: "Full-Stack Web Developer",
-    company_name: "FUNKASH",
-    period: "December 2024 - Present",
-    description:
-      "As a Full-Stack Web Developer at Funkash Family Holdings, I build and maintain scalable web applications across various ventures. I create intuitive interfaces using React and Tailwind CSS, and develop secure backend services with Node.js. I’ve worked on projects like Limpiar, enhancing usability and system efficiency. I deploy and manage applications on platforms like Render and Vercel, ensuring performance and scalability. I deliver impactful solutions aligned with business goals while optimizing workflows and implementing best practices in development.",
-    location: "Remote, Nigeria",
-  },
-  {
-    title: "Frontend Engineer",
-    company_name: "Knfrmd web corp",
-    period: "Feb 2023 - Nov 2024",
-    description:
-      "As a Frontend Web Developer at Knfrmd Web Corp., I bring my expertise in React, Next.js, TypeScript and my commitment to user-friendly design to the table.  Our team is dedicated to pushing the envelope in web development, ensuring seamless experiences across devices. With a skill set centered around front-end technologies and a passion for continuous learning, I am driven to contribute to projects that enhance productivity and user engagement. ",
-    location: "Remote, Canada",
-  },
-  {
-    title: "Web Developer",
-    company_name: "Rescue1 Youth Club",
-    period: "Aug 2024 - Nov 2024",
-    description:
-      "Tasked with completely re-building the user interface and managing content display, I successfully revamped the website to elevate the company's visibility through an improved user interface and optimized SEO configuration. Currently engaged in ongoing efforts to enhance the overall performance and usability of the web application, ensuring a top-notch experience for users.",
-    location: "Remote, USA",
-  },
-  {
-    title: "Frontend Developer",
-    company_name: "Taiwo Salam & Co. Properties LTD.",
-    period: "Aug 2024 - October 31 2024",
-    description:
-      "Played a pivotal role in the ongoing development of an in-house project, focusing on building user interfaces and managing dynamic content display. Leveraged state management and secure authentication integration to ensure a seamless user experience. As a key member of the frontend development team, I meticulously crafted interfaces that adhered to design specifications, significantly enhancing the project speed and overall efficiency.",
-    location: "Ibadan, Nigeria (Contract)",
-  },
-  {
-    title: "Frontend Developer",
-    company_name: "Jodna Technologies",
-    period: "June - August 8, 2024",
-    description:
-      "I collaborated with a team of developers to develop an investment application from the ground up, including design, development, testing, and deployment; conducted code reviews; contributed to a website redesign; optimized performance.",
-    location: "Remote, Nigeria",
-  },
-  {
-    title: "Web Developer & DevOps Engineer",
-    company_name: "SardionCare Inc.",
-    period: "Jun 2023 - Jul 2023",
-    description:
-      "I collaborated with another developer to conceptualize, design, and develop a website, including domain purchase, secure hosting, testing, CI/CD, and maintenance; conducted code reviews; contributed to a website redesign increasing user engagement by 15%; optimized performance for faster page loads, enhancing user experience.",
-    location: "Remote, Canada",
-  },
-  {
-    title: "Full Stack Web Developer Intern",
-    company_name: "Alusoft Technologies ltd.",
-    period: "Jan 2023 - Jun 2023",
-    description:
-      "Developed a Nodejs Web app exclusively for company use, streamlining the online management of students, staff and course allocation. Collaborated with other interns and an instructor to conduct user research and iterated on design for optimal functionality, resulting in a substantial increase in user satisfaction. Implemented the web application, achieving a more efficient working environment for staff.",
-    location: "Ibadan, Nigeria",
-  },
-];
-
-export const socialLinks = [
-  {
-    name: "GitHub",
-    link: "https://github.com/Oluwaferanmi-Dev",
-  },
-  {
-    name: "LinkedIn",
-    link: "https://ng.linkedin.com/in/feranmi-oyelowo",
-  },
-  {
-    name: "x",
-    link: "https://x.com/feranmi_sys?s=21",
-  },
-  {
-    name: "Email",
-    link: "mailto:feranmioyelowo@gmail.com",
-  },
-];
-
-export const skills = [
-  "TS/JS",
-  "NextJS/React",
-  "Node.Js",
-  "TailwindCSS",
-  "Docker",
-  "PostgreSQL",
-  "MongoDB",
-  "MySQL/MariaDB",
-  "Git/Github",
-  "Neovim/Vim",
-  "Linux",
-  "Wix",
-  "and more...",
-];
-
 export const projects = [
   {
     title: "Limpiar",
-    description: "Limpiar is a professional cleaning service that bridges the gap between property owners and cleaners.",
+    description:
+      "Limpiar is a full-service cleaning marketplace connecting property owners with vetted cleaning agencies. Visitors can log in using dummy data to explore the features as a property manager. This demo simulates the experience of finding and hiring cleaners in real time.",
     link: "https://limpiador-online.vercel.app/limpiador",
-    technologies: ["Typescript", "Node.js", "CSS", "Node.JS", "Express", "..."],
+    technologies: ["Typescript", "Node.js", "CSS", "Express", "..."],
     type: "live",
   },
   {
-    title: "My Portfolio",
+    title: "Limpiar Cleaning Agency",
     description:
-      "A personal portfolio website showcasing my projects, skills, and experiences.",
-    link: "https://oluwaferanmi-dev.vercel.app/",
-    technologies: ["TypeScript", "Next.js", "TailwindCSS"],
-    type: "live",
-  },
-  {
-    title: "Webpulse",
-    description: " A Real-time tracking, AI-driven optimization, and comprehensive insights to boost your websites speed and user experience.",
-    link: "https://webpulsev1.vercel.app/",
-    technologies: [
-      "TypeScript",
-      "Next.js",
-      "TailwindCSS",
-      "prisma",
-      "postgresql",
-      "docker",
-      "...",
-    ],
-    type: "live",
-  },
-  {
-    title: "Limpiar cleaning agency",
-    description:
-      "The cleaning agency interface for limpiar a professional cleaning service that bridges the gap between property owners and cleaners.",
+      "This interface allows cleaning agencies to manage jobs, update cleaning progress, and communicate with clients. You can log in with test/random credentials to explore how a cleaning company would manage operations on the platform.",
     link: "https://limpiar-cleaning-agency.vercel.app/",
     technologies: ["TypeScript", "Next.js", "TailwindCSS"],
     type: "live",
   },
   {
-    title: "QuickChat",
-    description: "A real-time chat application.",
-    link: "",
-    technologies: ["JavaScript", "Node.js", "React", "axios", "..."],
-    type: "",
-  },
-  {
-    title: "E-commerce-frontend",
-    description:
-      "A frontend for an e-commerce website built with React.js, to learn.",
-    link: "",
-    technologies: ["JavaScript", "React", "axios", "..."],
-    type: "",
-  },
-  
-  {
     title: "Limpiar Admins",
     description:
-      "The Admin interface for limpiar a professional cleaning service that bridges the gap between property owners and cleaners",
+      "Admin dashboard for overseeing the Limpiar platform. Includes user management, job tracking, and system analytics. You can explore the admin view with dummy data to understand platform-wide control.",
     link: "https://limpiar-admins-demo.vercel.app/",
     technologies: [
       "JavaScript",
@@ -159,6 +27,22 @@ export const projects = [
       "firebase",
       "axios",
       "notistack",
+      "...",
+    ],
+    type: "live",
+  },
+  {
+    title: "Webpulse",
+    description:
+      "A performance optimization dashboard offering real-time monitoring, AI-driven suggestions, and detailed insights to help developers boost website speed and user experience.",
+    link: "https://webpulsev1.vercel.app/",
+    technologies: [
+      "TypeScript",
+      "Next.js",
+      "TailwindCSS",
+      "prisma",
+      "postgresql",
+      "docker",
       "...",
     ],
     type: "live",
